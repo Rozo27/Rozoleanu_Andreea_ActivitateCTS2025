7e55ce511e7d739733a136af62c18be13c6232fa
Rozo27 <andreearozoleanu27@gmail.com>
2025-03-27 21:30:20 +0000

    Seminar 5

---
+++ seminar5/src/builderv2/IBuilder.java
@@ -0,0 +1,10 @@
+package builderv2;
+
+public interface IBuilder {
+    Rezervare buildRezervare(String numeClient);
+
+    IBuilder setAsezareGeam(Boolean asezareGeam);
+    IBuilder setScuaneErgonomice(Boolean scuaneErgonomice);
+    IBuilder setDecorareMasa(Boolean decorareMasa);
+    IBuilder setGenMuzica(String genMuzica);
+}
